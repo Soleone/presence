9ca6e382e1fcfb6cc9d2522500f100f66fc601c9
Soleone <dev@soleone.xyz>
2024-08-01 18:32:12 -0400

    Update games list for 2024

--- components/Games.tsx
+++ components/Games.tsx
@@ -1,6 +1,12 @@
 import { config } from '@/config';
 import Image from '@/components/Image';
 
+function Hype({ hype }: { hype: number }) {
+  return (
+    <></>
+  );
+};
+
 export default function Games() {
   const formatDate = (date: string) => {
     const options: Intl.DateTimeFormatOptions = { month: 'short', day: '2-digit', year: 'numeric' };
@@ -13,13 +19,14 @@ export default function Games() {
         Games coming out this year I am interested in.
       </p>
 
-      <div className='grid grid-cols-2 md:grid-cols-4'>
-        {config.extensions.games.map(({ title, description, date, image }) => (
+      <div className='grid grid-cols-1 md:grid-cols-4 gap-y-8'>
+        {config.extensions.games.map(({ title, description, date, image, hype }) => (
           <div key={title} className="flex flex-col items-center gap-2 group">
             <h2 className="font-bold text-lg">{title}</h2>
             <Image src={image} alt={title} isHoverable isClickable classNames='object-contain h-64 w-full' />
-            <p className="text-sm text-gray-300">{description}</p>
+            <p className="text-sm text-gray-300 h-8 text-center">{description}</p>
             <p className="text-sm group-hover:underline">{formatDate(date)}</p>
+            <Hype hype={hype} />
           </div>
         ))}
       </div>
--- config.ts
+++ config.ts
@@ -17,19 +17,93 @@ export const config = {
         date: "2024-08-20",
         description: "A Chinese Souls-like with impressive graphics.",
         image: "https://hyperpc.ae/images/company/promo/black-myth-wukong-promo/main/black-myth-wukong-mobile-banner.jpg",
+        hype: 3
       },
       {
         title: "Visions of Mana",
         date: "2024-08-29",
         description: "Successor to the classic SNES action rpg.",
         image: "https://m.media-amazon.com/images/M/MV5BNWU0ZmYxNjYtYzgyYi00ZjczLTljMmMtZTUyNDJhNjA4Yzc4XkEyXkFqcGdeQXVyMTk2OTAzNTI@._V1_.jpg",
+        hype: 1,
       },
       {
         title: "Star Wars: Outlaws",
         date: "2024-08-30",
         description: "Ubisoft open world Star Wars game.",
         image: "https://image.api.playstation.com/vulcan/ap/rnd/202306/3020/bca42b94af09b7a7ffd27fcb76a7bb1e46b60ed0c5f99322.png",
+        hype: 3,
       },
+      {
+        title: "Astro Bot",
+        date: "2024-09-06",
+        description: "Polished 3D platformer with hopefully lots of variety.",
+        image: "https://sm.ign.com/ign_nordic/cover/a/astro-bot/astro-bot_dzw7.jpg",
+        hype: 2,
+      },
+      {
+        title: "Warhammer 40,000: Space Marine 2",
+        date: "2024-09-09",
+        description: "Impressive visuals and potentially immersive combat gameplay.",
+        image: "https://image.api.playstation.com/vulcan/ap/rnd/202212/0711/MKweDX2LQpXiPcU6Jitg9KAg.png",
+        hype: 1,
+      },
+      {
+        title: "The Legend of Zelda: Echoes of Wisdom",
+        date: "2024-09-26",
+        description: "A new take on Zelda where we get to play as the princess.",
+        image: "https://scontent-yyz1-1.xx.fbcdn.net/v/t39.30808-6/448638547_906383094866034_4463467649343463158_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=127cfc&_nc_ohc=kE5Rh-6qCbYQ7kNvgGndfC0&_nc_ht=scontent-yyz1-1.xx&gid=Ax6B6I1CRl_Rc6TkPiXMRyb&oh=00_AYAEGnr_UgZ1sjaulzbQ0xc3jj3b3dnvCuHPBjPUPjvXGQ&oe=66B1C9E0",
+        hype: 3,
+      },
+      {
+        title: "Silent Hill 2",
+        date: "2024-10-08",
+        description: "Remake of the classic horror game. Not immensely hyped but definitely intrigued.",
+        image: "https://sm.ign.com/t/ign_nordic/cover/s/silent-hil/silent-hill-2-remake_rxcq.1200.jpg",
+        hype: 2,
+      },
+      {
+        title: "Metaphor ReFantazio",
+        date: "2024-10-11",
+        description: "New JRPG from the Persona creators, should be quite epic.",
+        image: "https://images.pushsquare.com/f8c81f2c373f9/metaphor-refantazio-ps5-box-art.large.jpg",
+        hype: 3,
+      },
+      {
+        title: "Call of Duty: Black Ops 6",
+        date: "2024-10-25",
+        description: "Interesting 80s conspiracy setting, could be an interesting short ride.",
+        image: "https://static0.gamerantimages.com/wordpress/wp-content/uploads/2024/06/call-of-duty-black-ops-6-tag-page-cover-art.jpg",
+        hype: 2,
+      },
+      {
+        title: "Mario & Luigi: Brothership",
+        date: "2024-11-07",
+        description: "A new entry in the series, hopefully some innovation and not too easy.",
+        image: "https://m.media-amazon.com/images/I/81YGfKshDwL.jpg",
+        hype: 2,
+      },
+      {
+        title: "Assassin's Creed Shadows",
+        date: "2024-11-12",
+        description: "Let's wait for the reviews to see if this is worth our time.",
+        image: "https://preview.redd.it/assassins-creed-shadows-official-cover-art-v0-yscvb4s38m0d1.jpeg?width=1080&crop=smart&auto=webp&s=6c6878e98512c39d83c6178e9da6e9f59977b06e",
+        hype: 1,
+      },
+      {
+        title: "Avowed",
+        date: "2024-11-12",
+        description: "Hopefully this has the RPG depths in gameplay and decisions that the studio is known for.",
+        image: "https://assets.xboxservices.com/assets/fa/12/fa1203f4-a126-41ff-ba6e-6b3e88ca0f19.jpg?n=49299933_GLP-Page-Hero-0_1083x1222_02.jpg",
+        hype: 2,
+      },
+      {
+        title: "STALKER 2: Heart of Chernobyl",
+        date: "2024-11-20",
+        description: "Open world atmospheric exploration.",
+        image: "https://m.media-amazon.com/images/M/MV5BMzEwNTg3NjMtMmJmYy00NmI4LWJkNmUtZDliNGM2MjJmOTgzXkEyXkFqcGdeQXVyMTk2OTAzNTI@._V1_FMjpg_UX1000_.jpg",
+        hype: 2,
+      },
+
     ]
   }
 };
